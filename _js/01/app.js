38078b1c680b5a8a6b5f8abccd93840d481de1f8
A-Kija <arvydas.kijakauskas@gmail.com>
2021-05-24 20:31:07 +0300

    js

---
+++ _js/01/app.js
@@ -0,0 +1,79 @@
+
+
+
+// console.log("------->", 9 + 9 + 9);
+
+// [1, 2, 8].forEach(val => console.log(val));
+
+// let ir const
+
+var a, e, f;
+var d = 45;
+var t = 45;
+var g = 3;
+
+// a = 1;
+console.log('d', d);
+
+d = 100;
+
+console.log('d', d);
+
+console.log(a);
+
+var b;
+
+
+
+console.log('+', d + t);
+
+console.log('-', d - t);
+
+console.log('*', d * t);
+
+console.log('/', d / t);
+
+console.log('**', d ** g);
+
+console.log('%', d % t);
+
+console.log('|++', d++); //// cia padideja
+
+console.log('++', d);
+
+console.log('++|', ++t);
+
+console.log('++', t);
+
+console.log('g', g--);
+
+console.log('g', g);
+
+// g = g + 7;
+
+g -= 7; //  g = g - 7;
+
+
+
+console.log('g+=', g);
+
+console.log('()', g + 6 * 2);
+
+var nn;
+var n = 'adsfdashgfdasdasdha-->';
+var n2 = '<--asdghasgdasghdasgdghasdvashgdashgdfghasfdghas';
+
+nn = n2 + n;
+
+
+// n++;
+
+console.log(typeof nn);
+console.log('n', nn);
+
+
+
+
+
+
+
