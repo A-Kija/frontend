4a944c3c41e2e9f27c7b7f63382cc218611a8e76
A-Kija <arvydas.kijakauskas@gmail.com>
2021-06-12 12:51:52 +0300

    withh id

---
+++ _js/18/app.js
@@ -0,0 +1,12 @@
+const allListOfAnimals = {}
+
+
+// {plesrunai: 1, zirafos: 2}
+
+// ul ciklas
+allListOfAnimals[ul.id] = 0;
+// ul -> li ciklas
+// count++
+// ul -> li ciklo pabaiga
+allListOfAnimals[ul.id] = count;
+// ul ciklo pabaiga
