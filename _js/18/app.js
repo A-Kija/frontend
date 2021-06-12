4832246a862a1117f0e04af27f836d0c52a4f9f7
A-Kija <arvydas.kijakauskas@gmail.com>
2021-06-12 13:21:54 +0300

    update

--- _js/18/app.js
+++ _js/18/app.js
@@ -4,9 +4,26 @@ const allListOfAnimals = {}
 // {plesrunai: 1, zirafos: 2}
 
 // ul ciklas
-allListOfAnimals[ul.id] = 0;
+// allListOfAnimals[ul.id] = 0;
 // ul -> li ciklas
 // count++
 // ul -> li ciklo pabaiga
-allListOfAnimals[ul.id] = count;
+// allListOfAnimals[ul.id] = count;
 // ul ciklo pabaiga
+
+
+
+const contacts = document.querySelector('#contacts');
+const x = contacts.querySelector('b');
+const did = contacts.querySelector('u');
+
+contacts.addEventListener('click', e => e.target.style.color = 'red');
+did.addEventListener('click', e => {
+    e.stopPropagation();
+    contacts.style.fontSize = '20px';
+    });
+
+    x.addEventListener('click', (e) => {
+        // e.stopPropagation();
+        contacts.style = null;
+    });  
