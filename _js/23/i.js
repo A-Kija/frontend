9b8d9ef237a2446d4d26b760be0bb6f8364c72d2
A-Kija <arvydas.kijakauskas@gmail.com>
2021-06-19 13:24:37 +0300

    u5

--- _js/23/i.js
+++ _js/23/i.js
@@ -7,7 +7,33 @@ console.log(document.querySelector('#_1').value));
 document.querySelector('#_2').addEventListener('change', event =>
 console.log(event.target.value));
 
+
+
 //3 
 document.querySelectorAll('[name=_3]').forEach(radio => {
     radio.addEventListener('change', event => console.log(event.target.value));
-})
+});
+
+
+//4
+document.querySelector('#_4').addEventListener('change', event => {
+    if (event.target.checked) {
+        console.log('Pažymėta');
+    }
+    else {
+        console.log('Nepažymėta');
+    }
+});
+
+
+//5
+document.querySelectorAll('[name=_5]').forEach(chEl => {
+    chEl.addEventListener('change', () => {
+        console.log('----------------------------');
+        document.querySelectorAll('[name=_5]').forEach(ch => {
+            if (ch.checked) {
+                console.log(ch.value);
+            }
+        });
+    });
+});
