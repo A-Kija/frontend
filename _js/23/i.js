650b6f5f5a3cc97636b652a5a8c458134cfda98d
A-Kija <arvydas.kijakauskas@gmail.com>
2021-06-19 11:46:40 +0300

    u2

--- _js/23/i.js
+++ _js/23/i.js
@@ -1,3 +1,8 @@
 //1
 document.querySelector('#button1').addEventListener('click', () =>
 console.log(document.querySelector('#_1').value));
+
+
+//2
+document.querySelector('#_2').addEventListener('change', sel =>
+console.log(sel.target.value));
