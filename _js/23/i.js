ca31f5bd9976951a1ba8e1de19f49a306b1ec1fe
A-Kija <arvydas.kijakauskas@gmail.com>
2021-06-21 20:29:30 +0300

    9

--- _js/23/i.js
+++ _js/23/i.js
@@ -1,17 +1,33 @@
+let arr1 = [];
+let arr2 = [];
+let arr3 = [];
+let arr4 = [];
+let arr5 = [];
 //1
-document.querySelector('#button1').addEventListener('click', () =>
-console.log(document.querySelector('#_1').value));
+document.querySelector('#button1').addEventListener('click', () => {
+    console.log(document.querySelector('#_1').value);
+    arr1.push(document.querySelector('#_1').value);
+    console.log(arr1);
+});
+
 
 
 //2
-document.querySelector('#_2').addEventListener('change', event =>
-console.log(event.target.value));
+document.querySelector('#_2').addEventListener('change', event => { 
+    console.log(event.target.value);
+    arr2.push(event.target.value);
+    console.log(arr2);
+});
 
 
 
 //3 
 document.querySelectorAll('[name=_3]').forEach(radio => {
-    radio.addEventListener('change', event => console.log(event.target.value));
+    radio.addEventListener('change', event => {
+        console.log(event.target.value);
+        arr3.push(event.target.value);
+        console.log(arr3);
+    });
 });
 
 
@@ -19,39 +35,57 @@ document.querySelectorAll('[name=_3]').forEach(radio => {
 document.querySelector('#_4').addEventListener('change', event => {
     if (event.target.checked) {
         console.log('Pažymėta');
+        arr4.push('Pažymėta');
+        console.log(arr4);
     }
     else {
         console.log('Nepažymėta');
+        arr4.push('Nepažymėta');
+        console.log(arr4);
     }
 });
 
 
 //5
-// document.querySelectorAll('[name=_5]').forEach(chEl => {
-//     chEl.addEventListener('change', () => {
-//         console.log('----------------------------');
-//         document.querySelectorAll('[name=_5]').forEach(ch => {
-//             if (ch.checked) {
-//                 console.log(ch.value);
-//             }
-//         });
-//     });
-// });
+document.querySelectorAll('[name=_5]').forEach(chEl => {
+    chEl.addEventListener('change', () => {
+        console.log('----------------------------');
+        const temp = [];
+        document.querySelectorAll('[name=_5]').forEach(ch => {
+            if (ch.checked) {
+                console.log(ch.value);
+                temp.push(ch.value);
+            }
+        });
+        arr5.push(temp);
+        console.log(arr5);
+    });
+});
 
+const bbz = (val) => {
+    console.log(val);
+}
 
 //6. 
 const h1 = document.querySelector('h1');
 
-
-document.querySelectorAll('[name=_5]').forEach(chEl => {
-    chEl.addEventListener('change', () => {
-        console.log('----------------------------');
-        let out = '';
-        document.querySelectorAll('[name=_5]').forEach(ch => {
-            if (ch.checked) {
-                out += ch.value + ' ';
-            }
-        });
-        h1.innerText = out;
+document.querySelector('#go').addEventListener('click', () => {
+    arr1.forEach((val) => {
+        console.log(val);
     });
+    arr2.forEach(bbz);
 });
+
+
+// document.querySelectorAll('[name=_5]').forEach(chEl => {
+//     chEl.addEventListener('change', () => {
+//         console.log('----------------------------');
+//         let out = '';
+//         document.querySelectorAll('[name=_5]').forEach(ch => {
+//             if (ch.checked) {
+//                 out += ch.value + ' ';
+//             }
+//         });
+//         h1.innerText = out;
+//     });
+// });
