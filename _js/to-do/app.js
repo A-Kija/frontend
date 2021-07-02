0d7de3f818e6b1f187c3871102adb588b337cff5
A-Kija <arvydas.kijakauskas@gmail.com>
2021-07-02 21:05:22 +0300

    to do list

--- _js/to-do/app.js
+++ _js/to-do/app.js
@@ -58,6 +58,8 @@ class ToDoListDOM {
 
     runInitCreators() {
         this.list(CREATED);
+        this.list(PROGRESS);
+        this.list(DONE);
     }
 
     //Creators
@@ -71,11 +73,40 @@ class ToDoListDOM {
             const li = document.createElement('li');
             const text = document.createTextNode(todo[1]);
             const id = document.createAttribute('data-id');
+            const button = document.createElement('button');
+            const deleteText = document.createTextNode('delete');
+            button.appendChild(deleteText);
             id.value = todo[0];
             li.setAttributeNode(id); 
             li.appendChild(text);
+            li.appendChild(button);
             place.appendChild(li);
+            this.deleteEvent(todo[0], type);
+            if (CREATED === type) {
+                this.toProgressEvent(todo[0]);
+            }
+            if (PROGRESS === type) {
+                this.toDoneEvent(todo[0]);
+            }
         }
+    }
+
+    confirmDelete(id, type) {
+        const div = document.createElement('div');
+        const text = document.createTextNode('Really?');
+        const deleteButton = document.createElement('button');
+        const cancelButton = document.createElement('button');
+        const deleteText = document.createTextNode('delete');
+        const cancelText = document.createTextNode('Nop');
+        deleteButton.appendChild(deleteText);
+        cancelButton.appendChild(cancelText);
+        div.appendChild(text);
+        div.appendChild(deleteButton);
+        div.appendChild(cancelButton);
+        div.classList.add('confirm');
+        document.body.appendChild(div);
+        this.deleteConfirmEvent(id, type, deleteButton, div);
+        this.cancelDeleteConfirmEvent(cancelButton, div);
     }
 
 
@@ -83,8 +114,57 @@ class ToDoListDOM {
     createEvent() {
         this.createButton.addEventListener('click', () => {
             ToDoList.create(this.createText.value);
+            this.createText.value = '';
+            this.list(CREATED);
         });
     }
+
+    deleteEvent(id, type) {
+        document.querySelector('[data-id="' + id + '"] button').addEventListener('click', () => {
+            this.confirmDelete(id, type);
+        })
+    }
+
+    deleteConfirmEvent(id, type, deleteButton, div) {
+        deleteButton.addEventListener('click', () => {
+            ToDoList.lists.get(type).list.delete(id);
+            ToDoList.lists.get(type).setListToStorage();
+            this.list(type);
+            div.remove();
+        })
+    }
+
+    cancelDeleteConfirmEvent(cancelButton, div) {
+        cancelButton.addEventListener('click', () => {
+            div.remove();
+        })
+    }
+
+    toProgressEvent(id) {
+        document.querySelector('[data-id="' + id + '"]').addEventListener('dblclick', () => {
+            const todo = ToDoList.lists.get(CREATED).list.get(id);
+            ToDoList.lists.get(CREATED).list.delete(id);
+            ToDoList.lists.get(CREATED).setListToStorage();
+            ToDoList.lists.get(PROGRESS).list.set(id, todo);
+            ToDoList.lists.get(PROGRESS).setListToStorage();
+            this.list(CREATED);
+            this.list(PROGRESS);
+        });
+    }
+
+    toDoneEvent(id) {
+        document.querySelector('[data-id="' + id + '"]').addEventListener('dblclick', () => {
+            const todo = ToDoList.lists.get(PROGRESS).list.get(id);
+            ToDoList.lists.get(PROGRESS).list.delete(id);
+            ToDoList.lists.get(PROGRESS).setListToStorage();
+            ToDoList.lists.get(DONE).list.set(id, todo);
+            ToDoList.lists.get(DONE).setListToStorage();
+            this.list(PROGRESS);
+            this.list(DONE);
+        });
+    }
+
+
 
 
 
