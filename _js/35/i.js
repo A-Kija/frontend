30cbebc76667d3cccedafdf06a15c4787d4dc680
A-Kija <arvydas.kijakauskas@gmail.com>
2021-07-03 13:43:08 +0300

    Sorter Class

---
+++ _js/35/i.js
@@ -0,0 +1,82 @@
+const mas1 = ['cat', 'bird', 'tiger', 'dog'];
+const set1 = new Set(['cat', 'bird', 'tiger', 'dog']);
+const map1 = new Map([['medium','cat'], ['small','bird'], ['big','tiger'], ['unknown','dog']]);
+
+console.log(mas1);
+console.log(set1);
+console.log(map1);
+
+//1
+// mas1.sort((a, b) => {
+//     if (a === b) {
+//         return 0;
+//     }
+//     return a < b ? -1 : 1;
+// });
+// console.log(mas1);
+// mas1.sort((a, b) => {
+//     if (a === b) {
+//         return 0;
+//     }
+//     return a > b ? -1 : 1;
+// })
+// console.log(mas1);
+
+//3
+class Sorter {
+
+    static sortAsc(data) {
+        if (data instanceof Array) {
+            return this.sortArrAsc(data)
+        }
+        if (data instanceof Set) {
+            return this.sortSetAsc(data)
+        }
+
+    }
+    static sortDesc(data) {
+        if (data instanceof Array) {
+            return this.sortArrDesc(data)
+        }
+        if (data instanceof Set) {
+            return this.sortSetDesc(data)
+        }
+    }
+
+    static sortSetAsc(set) {
+        const arr = this.sortArrAsc([...set]);
+        set.clear();
+        arr.forEach(v => set.add(v));
+        return set;
+    }
+
+    static sortSetDesc(set) {
+        const arr = this.sortArrDesc([...set]);
+        set.clear();
+        arr.forEach(v => set.add(v));
+        return set;
+    }
+
+    static sortArrAsc(arr) {
+        arr.sort((a, b) => {
+            if (a === b) {
+                return 0;
+            }
+            return a < b ? -1 : 1;
+        });
+        return arr;
+    }
+    static sortArrDesc(arr) {
+        arr.sort((a, b) => {
+            if (a === b) {
+                return 0;
+            }
+            return a > b ? -1 : 1;
+        });
+        return arr;
+    }
+}
+
+
+console.log(Sorter.sortAsc(mas1));
+console.log(Sorter.sortDesc(set1));
