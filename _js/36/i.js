f16a12b023de79dab5a696d8135351d216518450
A-Kija <arvydas.kijakauskas@gmail.com>
2021-07-05 19:49:53 +0300

    užd

---
+++ _js/36/i.js
@@ -0,0 +1,7 @@
+const json = '[{"id":1,"title":"MAC"},{"id":2,"title":"Nike"},{"id":3,"title":"Victoria\u2019s Secret"},{"id":4,"title":"Zara"},{"id":5,"title":"Zizzi"},{"id":6,"title":"Witteveen"},{"id":7,"title":"H&M"},{"id":8,"title":"RIMOWA"},{"id":9,"title":"Adidas"},{"id":10,"title":"Paw Patrol"},{"id":11,"title":"Mattel"},{"id":12,"title":"Kenzo"},{"id":13,"title":"Asos"}]';
+
+//A. Pagaminti Map tipo objektą, kuriame raktas yra id iš json'o stringo ir išvesti į konsolę
+
+
+//B. Pagaminti mygtuką HTML'e, kurį paspaudus iš json'o stringo pasidarytų Set tipo objektas su
+// reikšmėmis (kompanijų vardais) išrūšiuotais pagal abėcėlę ir išvesti setą į konsolę.
