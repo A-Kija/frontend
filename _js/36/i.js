aba1fb9952e749c44f6eb45a4376688d895bf7b1
A-Kija <arvydas.kijakauskas@gmail.com>
2021-07-08 18:40:31 +0300

    K2

--- _js/36/i.js
+++ _js/36/i.js
@@ -1,6 +1,8 @@
 //Kuku kazkas naujo
 // Voveres ir bebrai
 
+//BBBBBBBBBB
+
 const json = '[{"id":1,"title":"MAC"},{"id":2,"title":"Nike"},{"id":3,"title":"Victoria\u2019s Secret"},{"id":4,"title":"Zara"},{"id":5,"title":"Zizzi"},{"id":6,"title":"Witteveen"},{"id":7,"title":"H&M"},{"id":8,"title":"RIMOWA"},{"id":9,"title":"Adidas"},{"id":10,"title":"Paw Patrol"},{"id":11,"title":"Mattel"},{"id":12,"title":"Kenzo"},{"id":13,"title":"Asos"}]';
 
 const json1 = [{ id: 1, title: "MAC" }, { id: 2, title: "Nike" }, { id: 3, title: "Victoria\u2019s Secret" }, { id: 4, title: "Zara" }, { id: 5, title: "Zizzi" }, { id: 6, title: "Witteveen" }, { id: 7, title: "H&M" }, { id: 8, title: "RIMOWA" }, { id: 9, title: "Adidas" }, { id: 10, title: "Paw Patrol" }, { id: 11, title: "Mattel" }, { id: 12, title: "Kenzo" }, { id: 13, title: "Asos" }];
